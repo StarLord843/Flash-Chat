67ea676307aeac0168c440e2c4a50c19d1e7b3a1
Kaushal Kumar <kaushal.kumar@tatadigital.com>
2020-11-13 19:22:23 +0530

    some commenting in the final project

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -2,8 +2,8 @@
 //  RegisterViewController.swift
 //  Flash Chat iOS13
 //
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
+//  Created by Kaushal on 21/10/2019.
+//  Copyright © 2019 Kaushal Kumar. All rights reserved.
 //
 
 import UIKit
